6353a36f753b677b5002d994178530b4dbd45a26
ek <ek@EK-THINK>
2017-03-07 15:23:44 +0100

    Nu kan der sendes data til mysql database. Det virker dog kun over en VPN forbindelse stadig.

--- src/main/java/mySQLtest.java
+++ src/main/java/mySQLtest.java
@@ -1,4 +1,28 @@
+import java.sql.*;
 
-public class mySQLtest {
-
-}  
+public class mySQLtest
+{
+	private static String username = "javalogger";
+	
+	private static String password = "n5D1H0AAxVav0RXi";
+	
+	private static String connectionString = "jdbc:mysql://172.23.23.124:3306/log";
+	private static Connection connection;
+	private static Statement command;
+	private static ResultSet data;
+	
+	public static void main(String[] args) {
+		
+		try {
+			connection = DriverManager.getConnection(connectionString, username, password);
+			command = connection.createStatement();
+			command.execute("INSERT INTO `data` (`ID`, `MAC`, `timestamp`, `type`, `value`) VALUES (NULL, '1122334455667788', '1488642591', 'power', '123');");
+		//	Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	
+}
