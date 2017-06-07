3f5ccf395c3f55a260d6c45bc1d63b9ae958c68f
Emil Kristensen <eKristensen@users.noreply.github.com>
2017-06-07 15:41:47 +0200

    Add files via upload

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,234 +1,54 @@
-import com.google.gson.Gson;
-import com.rabbitmq.client.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Arrays;
 
-import java.sql.DriverManager;
-import java.util.UUID;
-import java.util.stream.IntStream;
+import com.rabbitmq.client.*;
 
 public class App {
-    private static final Logger log = LoggerFactory.getLogger(App.class);
-    private static Gson gson;
 
-    public static void main(String[] args) throws Exception {
-        gson = new Gson();
+	public static void main(String[] args) throws Exception {
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUsername("incap");
-        factory.setPassword("ORDZnBMCLH4BRYAAbdi1i3jTVonWozDE");
-        factory.setHost("broker.elektro.dtu.dk");
-        factory.setPort(5000);
-        factory.setVirtualHost("incap");
-        factory.setAutomaticRecoveryEnabled(true);
-        factory.useSslProtocol();
+		String addgate = null;
 
-        Connection connection = factory.newConnection();
-        Channel dataChannel = connection.createChannel();
-        Channel statusChannel = connection.createChannel();
-        Channel controlChannel = connection.createChannel();
+		if (args.length >= 1) {
+			System.out.println("Input registred. The gateway " + args[0] + " will be added.");
+			addgate = args[0];
+		} else {
+			System.out.println("No argument, datacollection starting...");
+		}
 
-        dataChannel.exchangeDeclare("data", "topic", true);
-        statusChannel.exchangeDeclare("status", "topic", true);
-        controlChannel.exchangeDeclare("control", "topic", true);
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUsername("incap");
+		factory.setPassword("ORDZnBMCLH4BRYAAbdi1i3jTVonWozDE");
+		factory.setHost("broker.elektro.dtu.dk");
+		factory.setPort(5000);
+		factory.setVirtualHost("incap");
+		factory.setAutomaticRecoveryEnabled(true);
+		factory.useSslProtocol();
 
-        String dataQueue = dataChannel.queueDeclare().getQueue();
-        String statusQueue = statusChannel.queueDeclare().getQueue();
-        // no queue "name" needed to publish to control
+		Connection connection = factory.newConnection();
+		Channel dataChannel = connection.createChannel();
+		Channel statusChannel = connection.createChannel();
+		Channel controlChannel = connection.createChannel();
 
-        dataChannel.queueBind(dataQueue, "data", "#");
-        statusChannel.queueBind(statusQueue, "status", "#");
-        // no binding needed to publish to control
+		dataChannel.exchangeDeclare("data", "topic", true);
+		statusChannel.exchangeDeclare("status", "topic", true);
+		controlChannel.exchangeDeclare("control", "topic", true);
 
-        // add listeners to data and status
-        Consumer dataConsumer = new Listener(dataChannel);
-        Consumer statusConsumer = new Listener(statusChannel);
-        // start listening to data and status
-        dataChannel.basicConsume(dataQueue, true, dataConsumer); // comment this line to receive less noise in output while testing commands
-        statusChannel.basicConsume(statusQueue, true, statusConsumer);
+		String dataQueue = dataChannel.queueDeclare().getQueue();
+		String statusQueue = statusChannel.queueDeclare().getQueue();
 
-        
-        
+		dataChannel.queueBind(dataQueue, "data", "#");
+		statusChannel.queueBind(statusQueue, "status", "#");
 
-        // Initialize database
-        // don't use this unless needed to, it won't help just blindly initializing the database, it will probably just mess stuff up
-//        new InitializeDatabase(controlChannel);
+		Consumer dataConsumer = new Listener(dataChannel, addgate);
+		Consumer statusConsumer = new Listener(statusChannel, null);
 
+		// dataChannel.basicConsume(dataQueue, true, dataConsumer);
+		statusChannel.basicConsume(statusQueue, true, statusConsumer);
 
-        // TESTS
-        // Configure a set of devices
-        //String owesome_gateway   = "0015BC001C0011A7";
-        //testDeleteUnit(controlChannel, owesome_gateway);
-        
-        //new InitGRP01(controlChannel);
-        mySQLtest.getLastTemp();
-        //testGetList(controlChannel, owesome_gateway); 
+		System.out.println(Arrays.toString(mySQLtest.GatewayList()));
+		System.out.println(Arrays.toString(mySQLtest.GatewayList()));
 
-        //AlgoritmeTest1.controlFridges(controlChannel);
-        
-        // Test LED
-        //testGreenLed(controlChannel, gateway, indicator);
-        //Thread.sleep(5000);
-        //testRedLed(controlChannel, gateway, indicator);
-        //Thread.sleep(5000);
-        //testTurnOffLeds(controlChannel, gateway, indicator);
-        //Thread.sleep(5000);
-        //testTurnOffLedsDelayed(controlChannel, gateway, indicator, 5000);
-        //Thread.sleep(10000);
-
-
-        // Test RELAY
-        //testSwitchRelay(controlChannel, gateway, relay, false);
-        //Thread.sleep(5000);
-        //testSwitchRelay(controlChannel, gateway, relay, true);
-
-
-        // Test API
-//        testAddUnit(controlChannel, gateway, relay, sensor, indicator); // add unit
-//        Thread.sleep(2000);
-//        testAddUnit(controlChannel, gateway, relay, sensor, indicator); // try adding it again to force a failure (watch the output from 'status')
-//        Thread.sleep(2000);
-//        testAddUnit(controlChannel, "0015BC0000000111", relay, sensor, indicator); // try adding new unit with a device that exists in other unit already
-//        Thread.sleep(2000);
-//        testAddUnit(controlChannel, "0015BC0000000222", "4YRH37FYRTUEJDYE", sensor, indicator); // try adding new unit with device with invalid addresses
-//        Thread.sleep(2000);
-//        testAddUnit(controlChannel, "0015BC000000XY", relay, sensor, indicator); // try adding new unit invalid gateway
-//        Thread.sleep(2000);
-//        testDeleteUnit(controlChannel, gateway); // delete unit
-//        Thread.sleep(2000);
-//        testDeleteUnit(controlChannel, gateway); // try deleting it again
-//        Thread.sleep(2000);
-//        testUpdateUnit(controlChannel, gateway, relay, sensor, indicator); // try updating a non-existing unit
-//        Thread.sleep(2000);
-//        testAddUnit(controlChannel, gateway, relay, sensor, indicator); // re-add unit
-//        Thread.sleep(2000);
-//        testUpdateUnit(controlChannel, gateway, relay, sensor, indicator); // update unit
-//        Thread.sleep(2000);
-//        testGetList(controlChannel, ApiCommand.EMPTY); // get at list of units registered
-//        Thread.sleep(2000);
-//        testGetList(controlChannel, gateway); // get a list of devices registered to a unit
-//        Thread.sleep(2000);
-//        testDeleteUnit(controlChannel, gateway); // clean up
-
-
-        while (true);
-//        dataChannel.close();
-//        statusChannel.close();
-//        controlChannel.close();
-//        connection.close();
-    }
-
-
-    private static void testGreenLed(Channel channel, String gateway, String indicator) throws Exception {
-        Command cmd = new Command("led", gateway, indicator);
-        cmd.addParameter("green", "on");
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testRedLed(Channel channel, String gateway, String indicator) throws Exception {
-        Command cmd = new Command("led", gateway, indicator);
-        cmd.addParameter("red", "on");
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testTurnOffLeds(Channel channel, String gateway, String indicator) throws Exception {
-        Command cmd = new Command("led", gateway, indicator);
-        cmd.addParameter("leds", "off");
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testTurnOffLedsDelayed(Channel channel, String gateway, String indicator, int delay) throws Exception {
-        Command cmd = new Command("led", gateway, indicator);
-        cmd.addParameter("red", "on");
-        //cmd.addItem("green", "on"); // seems only one led can be active at a time...
-        cmd.addParameter("delay", ""+delay);
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testSwitchRelay(Channel channel, String gateway, String relay, boolean state) throws Exception {
-        Command cmd = new Command("relay", gateway, relay);
-        cmd.addParameter("relay", Boolean.toString(state));
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-
-
-
-
-    private static void testAddUnit(Channel channel, String gateway, String relay, String sensor, String indicator) throws Exception {
-        ApiCommand cmd = new ApiCommand("add", gateway);
-        cmd.addDevice("relay", relay);
-        cmd.addDevice("sensor", sensor);
-        cmd.addDevice("indicator", indicator);
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testDeleteUnit(Channel channel, String gateway) throws Exception {
-        ApiCommand cmd = new ApiCommand("delete", gateway, null);
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testUpdateUnit(Channel channel, String gateway, String relay, String sensor, String indicator) throws Exception {
-        ApiCommand cmd = new ApiCommand("update", gateway);
-        cmd.addDevice("relay", "0015BC0000000AA1");
-        cmd.addDevice("sensor", "0015BC0000000AA2");
-        cmd.addDevice("indicator", "0015BC0000000AA3");
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testGetList(Channel channel, String gateway) throws Exception {
-        ApiCommand cmd = new ApiCommand("list", gateway, null);
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
-    private static void testAddUnitWithRules(Channel channel, String gateway, String relay, String sensor, String indicator, boolean read, boolean write, boolean config) throws Exception {
-        ApiCommand cmd = new ApiCommand("add", gateway);
-        cmd.setRules(new ApiCommand.Rules(read, write, config));
-        cmd.addDevice("relay", relay);
-        cmd.addDevice("sensor", sensor);
-        cmd.addDevice("indicator", indicator);
-        String correlation = UUID.randomUUID().toString();
-        cmd.setCorrelation(correlation);
-
-        String json = gson.toJson(cmd);
-        channel.basicPublish("control", "", null, json.getBytes());
-    }
-
+		// connection.close();
+	}
 
 }
--- src/main/java/mySQLtest.java
+++ src/main/java/mySQLtest.java
@@ -1,125 +1,121 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class mySQLtest
-{
+public class mySQLtest {
 	private static String username = "fagprojekt";
-	
+
 	private static String password = "gf3qAdOPH1l9YtSp";
-	
+
 	private static String connectionString = "jdbc:mysql://172.22.22.104:3306/fagprojekt";
 	private static Connection connection;
 	private static Statement cmd;
-	private static Statement command;
 	private static ResultSet data;
-	
-	private static int cachewhere = 0;
-	private static int cachesize = 20;
-	private static boolean sending = false;
-	private static Fridgedata[] cache = new Fridgedata[cachesize];
-	
+
+	private static long gatewaytime = 0;
+	private static String[] gatewaycache = new String[0];
+	private static long sendtime = 0;
+	private static ArrayList<Fridgedata> sendcache = new ArrayList<Fridgedata>();
+
 	public static void main(String[] args) {
 
 	}
-	
-	public static void sendTomySQL(Long timestamp, String gateway, String device, String topic, String signal, String value) throws java.lang.ClassNotFoundException {
-		
-		
-		
-		//add something to catch requets while sending to sql, while loop and a varriable saying its sending?
-/*		
-		while (sending) {
-			System.out.println("Sending: "+sending);
-		}
-		*/ // broken, must be fixed to get all data
-		if (cachewhere == cachesize) {
-			CommitCache();
-			sending = true;
-		}
-		
-		int i = cachewhere;
-		
 
-		cache[i] = new Fridgedata(timestamp, gateway, device, topic, signal, value, false);
-		
-		/*
-		System.out.println(i);
-		System.out.println(cache[0].getTopic());
-		
-		cache[i].setCommited(false);
-		cache[i].setTimestamp(timestamp);
-		cache[i].setGateway(gateway);
-		cache[i].setDevice(device);
-		cache[i].setTopic(topic);
-		cache[i].setSignal(signal);
-		cache[i].setValue(value);
-		*/
-		cachewhere++;
+	public static void sendTomySQL(Long timestamp, String gateway, String device, String topic, String signal,
+			String value) throws java.lang.ClassNotFoundException {
+		if ((sendtime + 1 * 60) <= (System.currentTimeMillis() / 1000L)) {
+			CommitCache();
+			sendtime = System.currentTimeMillis() / 1000L;
+		}
+		sendcache.add(new Fridgedata(timestamp, gateway, device, topic, signal, value, false));
 
 	}
-	
+
 	public static void CommitCache() throws java.lang.ClassNotFoundException {
 		try {
 			System.out.println("Sending");
 			connection = getConnection();
 			String sql = "INSERT INTO `data` (`ID`, `gateway`, `device`, `timestamp`, `topic`, `value`, `signaldb`) VALUES ";
-			
-			//System.out.println("ID1: "+cache[0].getTopic());
-			
-			for(int i = 0; i < cachesize; i++) {
-				//System.out.println("i is: " + i);
-				sql = sql + "(NULL, '"+cache[i].getGateway()+"'";
-				sql = sql + ", '"+cache[i].getDevice()+"'";
-				sql = sql + ", '"+cache[i].getTimestamp()/1000+"'";
-				sql = sql + ", '"+cache[i].getTopic()+"', '"+cache[i].getValue()+"', '"+cache[i].getSignal()+"')";
-				if (i != cachesize -1) sql = sql + ",";
-				cache[i].clear();
+
+			for (int i = 0; i < sendcache.size(); i++) {
+				sql = sql + "(NULL, '" + sendcache.get(i).getGateway() + "'";
+				sql = sql + ", '" + sendcache.get(i).getDevice() + "'";
+				sql = sql + ", '" + sendcache.get(i).getTimestamp() / 1000 + "'";
+				sql = sql + ", '" + sendcache.get(i).getTopic() + "', '" + sendcache.get(i).getValue() + "', '"
+						+ sendcache.get(i).getSignal() + "')";
+				if (i != sendcache.size() - 1)
+					sql = sql + ",";
+				sendcache.clear();
 			}
 			sql = sql + ";";
-			//System.out.println(sql);
-			command = connection.createStatement();
-			command.execute(sql);
-			cachewhere = 0;
-			sending = false;
+			connection.createStatement().executeQuery(sql); // send data
 			connection.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-		
-		
+		}
+
 	}
-	
+
+	public static String[] GatewayList() throws java.lang.ClassNotFoundException {
+		// System.out.println("nextupdate: "+(gatewaytime+ 5 * 60)+" current
+		// time: "+(System.currentTimeMillis() / 1000L));
+		if ((gatewaytime + 5 * 60) <= (System.currentTimeMillis() / 1000L)) {
+			try {
+				connection = getConnection();
+				String sql = "SELECT gate FROM `grupper`";
+				cmd = connection.createStatement();
+				data = cmd.executeQuery(sql);
+				ArrayList<String> fromsql = new ArrayList<String>();
+				if (data.first()) {
+					while (data.next()) {
+						fromsql.add(data.getString("gate"));
+					}
+				}
+
+				gatewaycache = fromsql.toArray(new String[fromsql.size()]);
+
+				gatewaytime = System.currentTimeMillis() / 1000L;
+				return gatewaycache;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+
+		} else {
+			return gatewaycache;
+		}
+
+	}
+
 	public static Connection getConnection() throws java.lang.ClassNotFoundException {
-	    try {
-	        Class.forName("com.mysql.jdbc.Driver");
-	        Connection conn = DriverManager.getConnection(connectionString, username, password);
-	        if (!conn.isClosed())
-	            return conn;
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(connectionString, username, password);
+			if (!conn.isClosed())
+				return conn;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
-	public static Data[] getLastTemp() throws SQLException,java.lang.ClassNotFoundException {
+
+	public static Data[] getLastTemp() throws SQLException, java.lang.ClassNotFoundException {
 		Connection connectionget = getConnection();
 		String gettemps = "SELECT t1.* FROM data t1 JOIN (SELECT device, MAX(timestamp) timestamp FROM data WHERE `topic` = 'temp' GROUP BY device) t2 ON t1.device = t2.device AND t1.timestamp = t2.timestamp WHERE `topic` = 'temp' ORDER BY `t1`.`value` DESC";
 		try {
 			cmd = connectionget.createStatement();
 			data = cmd.executeQuery(gettemps);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
 				int n = 0;
 				ArrayList<Data> fromsql = new ArrayList<Data>();
 				if (data.first()) {
-					while(data.next()) {
-						//System.out.println("device: " + data.getString("device") + " value: " + data.getString("value"));
+					while (data.next()) {
 						String gateway = data.getString("gateway");
-						fromsql.add(new Data(gateway,data.getString("device"),null,data.getInt("value"),true,500,200));
+						fromsql.add(new Data(gateway, data.getString("device"), null, data.getInt("value"), true, 500,
+								200));
 						n++;
 					}
 				}
@@ -128,38 +124,33 @@ public class mySQLtest
 				for (int i = 0; i < n; i++) {
 					String sensor = sqla[i].getSensor();
 					Statement cmd2 = connectionget.createStatement();
-					ResultSet data2 = cmd2.executeQuery("SELECT * FROM `data` WHERE `device` LIKE '"+sensor+"' ORDER BY `timestamp` ASC LIMIT 1");
+					ResultSet data2 = cmd2.executeQuery("SELECT * FROM `data` WHERE `device` LIKE '" + sensor
+							+ "' ORDER BY `timestamp` ASC LIMIT 1");
 					boolean state = true;
-					if (data2.first()){
-						//while (data2.next()) {
-							if (data2.getString("value").equals("false")) state = false;
-							sqla[i].setState(state);
-						//}
+					if (data2.first()) {
+						if (data2.getString("value").equals("false"))
+							state = false;
+						sqla[i].setState(state);
 					}
-					
-					
-					//hent relæ fra listen over enhedssæt i databasen!
 					Statement cmd3 = connectionget.createStatement();
-					System.out.println("SELECT * FROM `grupper` WHERE `sensor` LIKE '"+sensor+"' LIMIT 1");
-					ResultSet data3 = cmd3.executeQuery("SELECT * FROM `grupper` WHERE `sensor` LIKE '"+sensor+"' LIMIT 1");
+					System.out.println("SELECT * FROM `grupper` WHERE `sensor` LIKE '" + sensor + "' LIMIT 1");
+					ResultSet data3 = cmd3
+							.executeQuery("SELECT * FROM `grupper` WHERE `sensor` LIKE '" + sensor + "' LIMIT 1");
 					if (data3.first()) {
-						//while (data3.next()) {
-							System.out.println("hej, sat til: " + data3.getString("power"));
-							sqla[i].setRelay(data3.getString("power"));
-						//}
-					}					
+						System.out.println("hej, sat til: " + data3.getString("power"));
+						sqla[i].setRelay(data3.getString("power"));
+					}
 				}
 				return sqla;
-				
+
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 
 		connectionget.close();
 		return null;
-		
+
 	}
-	
+
 }
